b1e0c9f8547f25e1d7b463a1be9e651570ffbca1
root <ipanamski@gmail.com>
2015-11-10 23:07:19 +0200

    Added some more help.

--- commands.py
+++ commands.py
@@ -1,7 +1,8 @@
 print "Commands loaded!"
 buildversion = 1.0
 def help():
-	print "Help --> Shows this message."
-	print "Exit --> Exits."
+	print "Help    --> Shows this message."
+	print "Version --> Shows the current version."
+	print "Exit    --> Exits."
 def version():
 	print "Version: ", buildversion
--- main.py
+++ main.py
@@ -1,10 +1,15 @@
+import commands
 def main():
 	while (1 == 1):
 		command = raw_input('Command: ')
 		if(command == "help"):
-			print "This is help!";
+			commands.help()
 		elif(command == "exit"):
 			print "Exited!"
 			exit()
+		elif(command == "version"):
+			commands.version();
+		else:
+			print "There is no such command!"
 
 main()
